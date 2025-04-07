0af6f8f1e166b14d58a1ef09115e0a98161b3204
rengal3 <rengal3@gmail.com>
2025-04-07 07:19:06 -0500

    config inicial

---
+++ src/main/java/com/utp/utpproyectogit/UtpProyectoGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utp.utpproyectogit;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class UtpProyectoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
